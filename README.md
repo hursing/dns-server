33ef0faf8b52ea440eb8b455a7e34e8c717a9134
liuhengxing <vincent.liu@100cb.cn>
2018-07-26 18:26:20 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # dns-server
-A DNS server app written by Nodejs for developer and test use.
+A DNS server app written by pure Nodejs for develop and test use. Only support IPv4.
+
+纯Nodejs写的本地DNS服务器，供开发测试使用。前端做网站时，一般本地调试时的url是IP地址。但有些功能需要有域名才能工作，那么用这个工具来做本地解析就好了。
+
+https://tools.ietf.org/html/rfc1035
+
+## 使用方法How to use
+
+按照图示，找到原来的DNS服务器地址，并改成`127.0.0.1`
+
+### Mac
+
+![mac](image/mac-before.jpg)
+![mac](image/mac-after.jpg)
+
+### Windows
+
+![win](image/win-view-dns.jpg)
+![win](image/win-before.jpg)
+![win](image/win-after.jpg)
+
+打开`index.js`:
+
+![code](image/code.jpg)
+
+- 把fallbackServer改成原来的DNS服务器地址
+- 把domain改成你想解析的域名关键字。
+- 把targetIp改成你想解析到的IP地址
+
+
+然后用node运行`index.js`。mac上需要`sudo node index.js`。
---
+++ index.js
@@ -0,0 +1,100 @@
+// The keyword in domain name.
+const domain = 'hursing'
+// When keyword matched, resolve to this IP.
+const targetIp = '127.0.0.1'
+// When keyword not matched, use the fallback dns server to resolve.
+const fallbackServer = '10.0.0.1'
+
+const dgram = require('dgram')
+
+const server = dgram.createSocket('udp4')
+
+function copyBuffer(src, offset, dst) {
+  for (let i = 0; i < src.length; ++i) {
+    dst.writeUInt8(src.readUInt8(i), offset + i)
+  }
+}
+
+function resolve(msg, rinfo) {
+  const response = Buffer.alloc(28 + queryInfo.length)
+  let offset = 0
+  const id = msg.slice(0, 2)
+  copyBuffer(id, 0, response)  // Transaction ID
+  offset += id.length
+  response.writeUInt16BE(0x8180, offset)  // Flags
+  offset += 2
+  response.writeUInt16BE(1, offset)  // Questions
+  offset += 2
+  response.writeUInt16BE(1, offset)  // Answer RRs
+  offset += 2
+  response.writeUInt32BE(0, offset)  // Authority RRs & Additional RRs
+  offset += 4
+  const queryInfo = msg.slice(12)
+  copyBuffer(queryInfo, offset, response)
+  offset += queryInfo.length
+  response.writeUInt16BE(0xC00C, offset)  // offset to domain name
+  offset += 2
+  const typeAndClass = msg.slice(msg.length - 4)
+  copyBuffer(typeAndClass, offset, response)
+  offset += typeAndClass.length
+  response.writeUInt32BE(600, offset)  // TTL, in seconds
+  offset += 4
+  response.writeUInt16BE(4, offset)  // Length of IP
+  offset += 2
+  targetIp.split('.').map(value => {
+    response.writeUInt8(value, offset)
+    offset += 1
+  })
+  // console.log(response.toString('hex'))
+  server.send(response, rinfo.port, rinfo.address, (err) => {
+    if (err) {
+      console.log(err)
+      server.close()
+    }
+  })
+}
+
+function forward(msg, rinfo) {
+  const client = dgram.createSocket('udp4')
+  client.on('error', (err) => {
+    console.log(`client error:\n${err.stack}`)
+    client.close()
+  })
+  client.on('message', (fbMsg, fbRinfo) => {
+    server.send(fbMsg, rinfo.port, rinfo.address, (err) => {
+      err && console.log(err)
+    })
+    client.close()
+  })
+  client.send(msg, 53, fallbackServer, (err) => {
+    if (err) {
+      console.log(err)
+      client.close()
+    }
+  })
+}
+
+server.on('message', (msg, rinfo) => {
+  console.log(`receive from ${rinfo.address}:${rinfo.port}`)
+  // console.log(msg.toString('hex'))
+  const host = msg.slice(12).toString()
+  console.log(`host=${host}`)
+  if (host.indexOf(domain) === -1) {
+    forward(msg, rinfo)
+  } else {
+    resolve(msg, rinfo)
+  }
+})
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`)
+  server.close()
+})
+
+server.on('listening', () => {
+  const address = server.address()
+  console.log(`server listening ${address.address}:${address.port}`)
+})
+
+// On linux or Mac, run node with sudo. Because port 53 is lower then 1024.
+server.bind(53)
